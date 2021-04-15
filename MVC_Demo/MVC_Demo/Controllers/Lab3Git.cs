5a8cb8126ad281661ccfd3a207c3d0b55a30f617
damiequynh.24 <damiequynh.24@gmail.com>
2021-04-14 19:37:06 -0700

    Them 1 class va push len server

--- MVC_Demo/MVC_Demo/Controllers/Lab3Git.cs
+++ MVC_Demo/MVC_Demo/Controllers/Lab3Git.cs
@@ -7,9 +7,14 @@ public class Class1
 {
 	public Class1()
 	{
+<<<<<<< HEAD
 		//Sửa file commit
+=======
+>>>>>>> c0679035c885f39162290fc44b1ffb0deee1fc80
 		//
 		// TODO: Add constructor logic here
 		//
+
+		//Tiếp tục chỉnh sửa file
 	}
 }
